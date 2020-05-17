b23c660a9697f93bf8a92340cdc2a0b0a6d5da1d
Alexander <44812594+Scroodjke@aleks.lipckin@gmail.com>
2020-05-18 01:25:12 +0500

    first
    1

---
+++ D3D Framework/D3D_Framework/D3D_Framework.cpp
@@ -0,0 +1,11 @@
+﻿// D3D Framework.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "stdafx.h"
+#include "macros.h"
+
+// TODO: Это пример библиотечной функции.
+void fnD3DFramework()
+{
+
+}
